4ac0b6ffc4bb209658df1e9a9f84fb90bd3e12d6
dengweijian <892219254@qq.com>
2021-11-09 07:41:55 +0800

    four Commit of ScalaTest module

---
+++ src/main/Scala/testGit/testBranch.scala
@@ -0,0 +1,13 @@
+package testGit
+
+/**
+ * @Auther:weijian
+ * @Date:2021/11/9-11-09-7:39
+ * @Description:testGit
+ * @version:1.0
+ */
+object testBranch {
+  def main(args: Array[String]): Unit = {
+    println("this is master")
+  }
+}
